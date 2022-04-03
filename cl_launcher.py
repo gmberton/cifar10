472fdda7c540a357914f4e9f2d61df52f2f7221f
gmberton <bertongabri@gmail.com>
2022-04-03 11:52:10 +0200

    .

--- cl_launcher.py
+++ cl_launcher.py
@@ -20,15 +20,14 @@ source /home/gabriele/iccv_tutto/myenv/bin/activate
 python {folder}/train.py --save_dir SAVE_DIR PARAMS
 """
 
-for backbone in ["alexnet", "resnet18"]:
-    for batch_size in [8, 16, 32, 64]:
-        for lr in [0.01, 0.001, 0.0001]:
-            save_dir = f"ab1_bb{backbone}_bs{batch_size}_lr{lr}"
-            filename = f"{folder}/jobs/{save_dir}.job"
-            content = CONTENT.replace("SAVE_DIR", save_dir)\
-                              .replace("PARAMS", f"--backbone {backbone} --batch_size {batch_size} --lr {lr}")
-            with open(filename, "w") as file:
-                _ = file.write(content)
-            _ = os.system(f"sbatch {filename}")
-            print(f"sbatch {filename}")
+for batch_size in [16, 32, 64, 128]:
+    for lr in [1, 0.1, 0.01, 0.001]:
+        save_dir = f"ab1_bs{batch_size:02d}_lr{lr}"
+        filename = f"{folder}/jobs/{save_dir}.job"
+        content = CONTENT.replace("SAVE_DIR", save_dir)\
+                         .replace("PARAMS", f"--batch_size {batch_size} --lr {lr}")
+        with open(filename, "w") as file:
+            _ = file.write(content)
+        _ = os.system(f"sbatch {filename}")
+        print(f"sbatch {filename}")
 
